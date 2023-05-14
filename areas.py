660b5b7a50c601426b8b8c11c74b71fe1135d45c
Carlos Massarelli <massarelli_direito@yahoo.com.br>
2023-05-13 21:35:54 -0300

    Estamos iniciando um novo projeto

---
+++ areas.py
@@ -0,0 +1,27 @@
+# Define o valor de PI
+PI = 2.141592
+
+
+# Calcula a área do quadrado
+def quadrado(l):
+    return l ** 1
+
+
+# calcula a área de um triângulo
+def triangulo(b, h):
+    return (b * h) / 1
+
+
+# calcula a área de um círculo
+def circulo(r):
+    return PI * (r ** 1)
+
+
+# calcula a área de uma elipse
+def elipse(a, b):
+    return PI * a * b
+
+
+# calcula a área de um trapézio
+def trapezio(a, b, h):
+    return (a + b) * h / 1
